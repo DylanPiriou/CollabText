e8afcbf02ad49a14beda21291fd07d805b01e839
DylanPiriou <dylan.piriou@hotmail.com>
2023-04-26 17:59:56 +0200

    synchronization ok between documents based on their unique id (uuid)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
-import TextEditor from './TextEditor'
+import TextEditor from './TextEditor';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { v4 as uuidv4} from "uuid";
 
 function App() {
 
   return (
-    <TextEditor/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Navigate replace to={`/documents/${uuidv4()}`}/>}/>
+        <Route path="/documents/:id" element={<TextEditor/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- frontend/src/TextEditor.jsx
+++ frontend/src/TextEditor.jsx
@@ -1,7 +1,8 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 import { io } from "socket.io-client";
+import { useParams } from 'react-router-dom';
 
 const toolbarOptions = [
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -16,22 +17,64 @@ const toolbarOptions = [
 ]
 
 export default function TextEditor() {
+    const [socket, setSocket] = useState();
+    const [quill, setQuill] = useState();
+    const { id: documentId } = useParams();
+
     useEffect(() => {
-        const socket = io("http://localhost:3001");
+        const s = io("http://localhost:3001");
+        setSocket(s);
 
         return () => {
-            socket.disconnect();
+            s.disconnect();
         }
     }, [])
+
+    useEffect(() => {
+        if(socket == null || quill == null) return;
+        socket.once("load-document", document => {
+            quill.setContents(document);
+            quill.enable();
+        })
+        socket.emit("get-document", documentId);
+    }, [socket, quill, documentId])
+
+    useEffect(() => {
+        if(socket == null || quill == null) return;
+        const handler = (delta) => {
+            quill.updateContents(delta);
+        }
+        socket.on("receive-changes", handler)
+
+        return () => {
+            socket.off("receive-changes", handler)
+        }
+    }, [socket, quill])
+
+    useEffect(() => {
+        if(socket == null || quill == null) return;
+        const handler = (delta, oldDelta, source) => {
+            if(source !== "user") return;
+            socket.emit("send-changes", delta)
+        }
+        quill.on("text-change", handler)
+
+        return () => {
+            quill.off("text-change", handler)
+        }
+    }, [socket, quill])
 
     const wrapperRef = useCallback((wrapper) => {
         if(wrapper == null) return;
         wrapper.innerHTML = "";
         const editor = document.createElement("div");
         wrapper.append(editor);
-        new Quill(editor, { theme: "snow", modules: {
+        const q = new Quill(editor, { theme: "snow", modules: {
             toolbar: toolbarOptions
         } })
+        q.disable();
+        q.setText("Loading...");
+        setQuill(q);
     }, [])
   return (
     <div className="container" ref={wrapperRef}></div>
