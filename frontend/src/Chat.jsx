4df7c1fe9f09e27b077a975cdd8faa0af12dcf21
DylanPiriou <dylan.piriou@hotmail.com>
2023-04-30 16:03:06 +0200

    send button added to chat

--- frontend/src/Chat.jsx
+++ frontend/src/Chat.jsx
@@ -4,14 +4,13 @@ import ScrollToBottom from "react-scroll-to-bottom";
 
 export default function Chat({ username, socket, documentId }) {
     const [isOpen, setIsOpen] = useState(true);
-    const btn = useRef();
     const input = useRef();
 
     const [currentMessage, setCurrentMessage] = useState("");
     const [chat, setChat] = useState([]);
 
     const sendMessage = async (e) => {
-        if (currentMessage !== "" && currentMessage.trim().length !== 0 && e.key === "Enter") {
+        if (currentMessage !== "" && currentMessage.trim().length !== 0) {
             const messageData = {
                 room: documentId,
                 userId: socket.id,
@@ -36,40 +35,47 @@ export default function Chat({ username, socket, documentId }) {
     }
 
     return (
-        <div className="chat">
+        <>
             {isOpen ? (
                 <div className="chat-icon" onClick={() => handleChat()}>
                     ☎
                 </div>
             ) : (
-                <ScrollToBottom className="chat-content">
-                    <ul className="message-area">
-                        {chat.map((content, index) => {
-                            return (
-                                <li className="message" id={username === content.username ? "me" : "other"} key={`message-${index}`}>
-                                    <div className="content-wrapper">
-                                        <div className="content">{content.message}</div>
-                                        <div className="data">{content.username} - {content.time}</div>
-                                    </div>
-                                </li>
-                            )
-                        })}
-                    </ul>
-                    <input
-                        type="text"
-                        name=""
-                        id=""
-                        placeholder="Let's talk !"
-                        value={currentMessage}
-                        ref={input}
-                        onChange={(e) => setCurrentMessage(e.target.value)}
-                        onKeyDown={(e) => sendMessage(e)}
-                    />
-                    <span ref={btn} onClick={() => handleChat()}>
-                        X
-                    </span>
-                </ScrollToBottom>
+                <>
+                    <div className="chat-icon" onClick={() => handleChat()}>
+                        x
+                    </div>
+                <div className="chat">
+                    <ScrollToBottom className="chat-content">
+                        <ul className="message-area">
+                            {chat.map((content, index) => {
+                                return (
+                                    <li className="message" id={username === content.username ? "me" : "other"} key={`message-${index}`}>
+                                        <div className="content-wrapper">
+                                            <div className="content">{content.message}</div>
+                                            <div className="data">{content.username} - {content.time}</div>
+                                        </div>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                        <div className="input-wrapper">
+                            <input
+                                type="text"
+                                name=""
+                                id=""
+                                placeholder="Let's talk !"
+                                value={currentMessage}
+                                ref={input}
+                                onChange={(e) => setCurrentMessage(e.target.value)}
+                                onKeyDown={(e) => {e.key === "Enter" && sendMessage(e)}}
+                            />
+                            <button onClick={() => sendMessage()}>►</button>
+                        </div>
+                    </ScrollToBottom>
+                </div>
+                </>
             )}
-        </div>
+        </>
     );
 }
